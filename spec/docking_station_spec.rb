82e1d1dd1f16b3036ab0e3ecf552d56af573dbfc
Alex <alex.satur89@gmail.com>
2017-09-30 13:05:28 +0100

    doubles added, tests failing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,15 +9,14 @@ describe DockingStation do
 
   it 'only releases working bikes' do
     station = DockingStation.new
-    station.dock(Bike.new)
+    station.dock(double :bike)
     released_bike = station.release_bike
     expect(released_bike.bike_is_working?).to eq true
   end
 
   it 'can dock bikes' do
-    bike = Bike.new
     station = DockingStation.new
-    station.dock(bike)
+    station.dock(double :bike)
     expect(station.bikes).to include bike
   end
 
@@ -30,12 +29,12 @@ describe DockingStation do
     station = DockingStation.new
     10.times do station.dock(double :bike)
     end
-    expect {station.dock(Bike.new)}.to raise_error("No more space")
+    expect {station.dock(double :bike)}.to raise_error("No more space")
   end
 
   it 'will not release a broken bike' do
     station = DockingStation.new
-    bike = Bike.new
+    bike = (double :bike)
     bike.report_as_broken
     station.dock(bike)
     expect {station.release_bike}.to raise_error("No bikes available")
@@ -43,9 +42,9 @@ describe DockingStation do
 
   it 'can return the status of all bikes docked there' do
     station = DockingStation.new
-    6.times do station.dock(Bike.new)
+    6.times do station.dock(double :bike)
     end
-    4.times do station.dock((Bike.new).report_as_broken)
+    4.times do station.dock((double :bike).report_as_broken)
     end
     expect(station.bike_count).to eq "6 working bikes, 4 broken bikes"
   end
